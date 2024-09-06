665917279757e35e508e5df35f5e27db617dc129
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-09-06 19:41:11 +0900

    インクリメント、ディクリメント

--- Lesson09_08.java
+++ Lesson09_08.java
@@ -1,10 +1,11 @@
 public class Lesson09_08 {
     public static void main(String[] args){
         int x = 8;
-        int y = 12;
-        int z = 20;
+        int y = 8;
+        x++;
+        y--;
 
-        System.out.println(x += 10);
-        System.out.println(z += y);
+        System.out.println(x);
+        System.out.println(y);
     }
 }
