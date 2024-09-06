9d1700aec76f135e0f377af27251f21dd1432ba6
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-09-07 08:50:07 +0900

    文字列変更

--- sekachan/FirstComment.java
+++ sekachan/FirstComment.java
@@ -6,6 +6,6 @@ package sekachan;
 
    class FirstComment{
     public static void main(String[] args){
-      System.out.println("Javaを楽しもう！");  // 画面表示
+      System.out.println("Javaを動きました！");  // 画面表示
     }
   }
