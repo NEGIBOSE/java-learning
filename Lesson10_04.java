91483aeed26bca980496b566cce25e5dc4c234d4
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-09-06 23:26:30 +0900

    if ~ else 文

---
+++ Lesson10_04.java
@@ -0,0 +1,12 @@
+public class Lesson10_04 {
+    public static void main(String[] args){
+        int age = 22;
+
+        if (age >=20){
+            System.out.println("adult");
+        } else {
+            System.out.println("child");
+        }
+
+    }
+}
